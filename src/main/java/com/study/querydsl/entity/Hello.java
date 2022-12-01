0f6f65de4933ac124236a81d9fad2b61d7467cf6
ryujeongju <jeongju_ryu@tmax.co.kr>
2022-12-01 12:27:25 +0900

    [config] QueryDsl 기본 세팅 추가

--- src/main/java/com/study/querydsl/entity/Hello.java
+++ src/main/java/com/study/querydsl/entity/Hello.java
@@ -1,2 +1,21 @@
-package com.study.querydsl.entity;public class Hello {
+package com.study.querydsl.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hello {
+    @Id  @GeneratedValue
+    private Long id;
 }
--- src/test/java/com/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,31 @@
 package com.study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entity.Hello;
+import com.study.querydsl.entity.QHello;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
+    @Autowired
+    EntityManager em;
     @Test
     void contextLoads() {
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+        Hello hello = new Hello();
+        QHello hello1 = QHello.hello;
+        em.persist(hello);
+        Hello hello2 = jpaQueryFactory.selectFrom(hello1).fetchOne();
+        Assertions.assertThat(hello2).isEqualTo(hello);
     }
 
 }
